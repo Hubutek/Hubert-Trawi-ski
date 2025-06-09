8f00cf38e74f7d1eabf5c6602621f69ed80f6c45
Hubutek <251338@edu.p.lodz.pl>
2025-06-09 14:39:22 +0200

    Add files via upload

---
+++ Figury 1/Rectangle.h
@@ -0,0 +1,26 @@
+//
+// Created by Rafal Nowak on 28/05/2022.
+//
+
+#ifndef INHERITANCE_RECTANGLE_H
+#define INHERITANCE_RECTANGLE_H
+
+
+#include "Figure.h"
+
+class Rectangle : public Figure
+{
+private:
+    float m_a;
+    float m_b;
+
+public:
+    Rectangle(string name = "", float a = 0, float b = 0);
+    virtual ~Rectangle() {}
+    virtual float Area() const;
+    virtual float Perimeter() const;
+    virtual void Info() const;
+};
+
+
+#endif //INHERITANCE_RECTANGLE_H
